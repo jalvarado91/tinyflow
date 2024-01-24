68eac8ae0a0a3382828d60eb33579220c6bf2530
Juan Alvarado <jalvarado91@gmail.com>
2024-01-24 13:53:05 -0500

    Layout Nodes

--- src/app/_components/flowboard.tsx
+++ src/app/_components/flowboard.tsx
@@ -4,12 +4,18 @@ import { useCallback } from "react";
 import ReactFlow, {
   Background,
   Controls,
-  MiniMap,
   useNodesState,
   useEdgesState,
   addEdge,
   Position,
+  ReactFlowProvider,
+  useReactFlow,
+  Panel,
+  type Edge,
+  type Connection,
+  type Node,
 } from "reactflow";
+import Dagre from "@dagrejs/dagre";
 
 import "reactflow/dist/style.css";
 
@@ -20,28 +26,42 @@ const nodeDefaults = {
 
 const initialNodes = [
   {
+    ...nodeDefaults,
     id: "1",
-    position: { x: 0, y: 150 },
+    position: {
+      x: 75,
+      y: 65,
+    },
     data: { label: "default style 1" },
-    ...nodeDefaults,
+    type: "input",
   },
   {
+    ...nodeDefaults,
     id: "2",
-    position: { x: 250, y: 0 },
+    position: {
+      x: 275,
+      y: 20,
+    },
     data: { label: "default style 2" },
-    ...nodeDefaults,
   },
   {
+    ...nodeDefaults,
     id: "3",
-    position: { x: 250, y: 150 },
+    position: {
+      x: 275,
+      y: 110,
+    },
     data: { label: "default style 3" },
-    ...nodeDefaults,
   },
   {
-    id: "4",
-    position: { x: 250, y: 300 },
-    data: { label: "default style 4" },
     ...nodeDefaults,
+    id: "4",
+    position: {
+      x: 475,
+      y: 65,
+    },
+    data: { label: "Output" },
+    type: "output",
   },
 ];
 
@@ -50,7 +70,6 @@ const initialEdges = [
     id: "e1-2",
     source: "1",
     target: "2",
-    animated: true,
   },
   {
     id: "e1-3",
@@ -58,20 +77,75 @@ const initialEdges = [
     target: "3",
   },
   {
-    id: "e1-4",
-    source: "1",
+    id: "e2-e4",
+    source: "2",
+    target: "4",
+  },
+  {
+    id: "e3-e4",
+    source: "3",
     target: "4",
   },
 ];
 
+const g = new Dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
+
+const getLayoutedElements = (
+  nodes: Node<
+    {
+      label: string;
+    },
+    string | undefined
+  >[],
+  edges: Edge<any>[],
+) => {
+  g.setGraph({ rankdir: "LR" });
+
+  edges.forEach((edge) => g.setEdge(edge.source, edge.target));
+  nodes.forEach((node) => g.setNode(node.id, node));
+
+  Dagre.layout(g);
+
+  return {
+    nodes: nodes.map((node) => {
+      const { x, y } = g.node(node.id);
+
+      return { ...node, position: { x, y } };
+    }),
+    edges,
+  };
+};
+
 export function FlowBoard() {
+  return (
+    <ReactFlowProvider>
+      <LayoutFlow />
+    </ReactFlowProvider>
+  );
+}
+
+export function LayoutFlow() {
+  const { fitView } = useReactFlow();
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
   const onConnect = useCallback(
-    (params) => setEdges((els) => addEdge(params, els)),
-    [],
+    (params: Edge | Connection) => setEdges((els) => addEdge(params, els)),
+    [setEdges],
   );
+
+  const onLayout = useCallback(() => {
+    const layouted = getLayoutedElements(nodes, edges);
+
+    console.log({ layouted });
+
+    setNodes([...layouted.nodes]);
+    setEdges([...layouted.edges]);
+
+    window.requestAnimationFrame(() => {
+      fitView();
+    });
+  }, [nodes, edges, setNodes, setEdges, fitView]);
 
   return (
     <ReactFlow
@@ -81,8 +155,12 @@ export function FlowBoard() {
       onEdgesChange={onEdgesChange}
       proOptions={{ hideAttribution: true }}
       onConnect={onConnect}
+      onLoad={onLayout}
       fitView
     >
+      <Panel position="top-right">
+        <button onClick={() => onLayout()}>horizontal layout</button>
+      </Panel>
       <Background />
       <Controls />
     </ReactFlow>
